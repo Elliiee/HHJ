28386019cfbb67c8cff659026f232c3f196b4265
Elliiee <danyiluozhang@gmail.com>
2020-02-21 10:11:58 -0600

    1302

---
+++ Tree/src/_1302DeepestLeavesSum.java
@@ -0,0 +1,20 @@
+import java.util.LinkedList;
+
+public class _1302DeepestLeavesSum {
+    public int deepestLeavesSum(TreeNode root){
+        int res = 0, i;
+        LinkedList<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while (!q.isEmpty()){
+            for (i = q.size() - 1, res = 0; i >= 0; i--){
+                TreeNode node = q.poll();
+                res += node.val;
+                if (node.right != null)
+                    q.add(node.right);
+                if (node.left != null)
+                    q.add(node.left);
+            }
+        }
+        return res;
+    }
+}
