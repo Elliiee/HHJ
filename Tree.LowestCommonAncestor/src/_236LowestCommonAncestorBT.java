0d21cb241a13a6cde76dbb0b6b15f8714120b57e
Elliiee <danyiluozhang@gmail.com>
2020-02-25 10:12:22 -0600

    236

--- Tree.LowestCommonAncestor/src/_236LowestCommonAncestorBT.java
+++ Tree.LowestCommonAncestor/src/_236LowestCommonAncestorBT.java
@@ -1,15 +1,63 @@
+import java.util.*;
+
 public class _236LowestCommonAncestorBT {
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q){
-        if (root == null || root == p || root == q)
-            return root;
+    private TreeNode ans;
 
-        TreeNode left = lowestCommonAncestor(root.left, p, q);
-        TreeNode right = lowestCommonAncestor(root.right, p, q);
+    public _236LowestCommonAncestorBT() {
+        this.ans = null; // variable to store LCA node
+    }
 
-        if (left != null && right != null){
-            return root;
-        } else {
-            return left == null ? right : left;
+    private boolean recurseTree(TreeNode currentNode, TreeNode p, TreeNode q){
+        if (currentNode == null) return false; // reach the end
+
+        int left = this.recurseTree(currentNode.left, p, q) ? 1 : 0;
+        int right = this.recurseTree(currentNode.right, p, q) ? 1 : 0;
+
+        int mid = (currentNode == p || currentNode == q) ? 1 : 0;
+
+        if (mid + left + right >= 2){
+            this.ans = currentNode;
         }
+
+        return (mid + left + right > 0);
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q){
+        this.recurseTree(root, p, q);
+        return this.ans;
+    }
+
+    public TreeNode lowestCommonAncestorII(TreeNode root, TreeNode p, TreeNode q){
+        Map<TreeNode, TreeNode> parent = new HashMap<>();
+        Deque<TreeNode> stack = new ArrayDeque<>();
+
+        parent.put(root, null);
+        stack.push(root);
+
+        while (!parent.containsKey(p) || !parent.containsKey(q)){
+            TreeNode node = stack.pop();
+
+            if (node.left != null){
+                parent.put(node.left, node);
+                stack.push(node.left);
+            }
+            if (node.right != null){
+                parent.put(node.right, node);
+                stack.push(node.right);
+            }
+        }
+
+        // Ancestors set() for node p.
+        Set<TreeNode> ancestors = new HashSet<>();
+
+        while (p != null){
+            ancestors.add(p);
+            p = parent.get(p);
+        }
+
+        while (!ancestors.contains(q)){
+            q = parent.get(q);
+        }
+        return q;
     }
 }
